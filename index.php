85be1b102494bc6110d6d5306af71188f4514897
Scarlett PAJEO <spajeo@e3r2p17.42.fr>
2018-03-31 20:49:03 +0200

    admin add/modif

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 <?php
 
 /* ===== Each book category has its one and only price ===== */
-if (isset($_POST['cart']) && $_POST['cart'] === "Add to cart") {
+if (isset($_POST['cart']) && $_POST['cart'] === 'Add to cart') {
     if (!isset($_SESSION['cart_amount']))
         $_SESSION['cart_amount'] = 0;
-    $_SESSION['product_price'] = ""; /* Need a function there that could get the price of the item, depending on the name POSTed */
+    $books = unserialize(file_get_contents('./db/books'));
+    //$books[$_POST['title']]['price'];
+
+    $_SESSION['product_price'] = $books[$_POST['title_to_add']]['price']; /* Need a function there that could get the price of the item, depending on the name POSTed */
     header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
@@ -30,7 +33,10 @@ if (isset($_POST['empty_cart']) && $_POST['empty_cart'] === "Empty") {
         <?php include ('main_page.php'); ?>
         <?php include ('footer.php'); ?>
     <form action="index.php" method="POST">
+      <input type="hidden" name="title_to_add" value="fanfan la tulipe" /><br />
         <input type="submit" name="cart" value="Add to cart" /><br />
+    </form>
+    <form action="index.php" method="POST">
         <input type="submit" name="empty_cart" value="Empty" />
     </form>
 
