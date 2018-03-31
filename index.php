d7e2b22057988a91bfc814375b0167df86f70223
Fabien RAHAINGOMANANA <frahaing@e3r2p19.42.fr>
2018-03-31 20:06:54 +0200

    Added user management

--- index.php
+++ index.php
@@ -2,28 +2,10 @@
 <?php
 
 /* ===== Each book category has its one and only price ===== */
-if (isset($_POST['thrillers']) && $_POST['thrillers'] === "Add to cart") {
+if (isset($_POST['cart']) && $_POST['cart'] === "Add to cart") {
     if (!isset($_SESSION['cart_amount']))
         $_SESSION['cart_amount'] = 0;
-    $_SESSION['product_price'] = 5.2;
-    header('Location: ' . $_SERVER['PHP_SELF']);
-}
-if (isset($_POST['novels']) && $_POST['novels'] === "Add to cart") {
-    if (!isset($_SESSION['cart_amount']))
-        $_SESSION['cart_amount'] = 0;
-    $_SESSION['product_price'] = 4.6;
-    header('Location: ' . $_SERVER['PHP_SELF']);
-}
-if (isset($_POST['sf']) && $_POST['sf'] === "Add to cart") {
-    if (!isset($_SESSION['cart_amount']))
-        $_SESSION['cart_amount'] = 0;
-    $_SESSION['product_price'] = 2.8;
-    header('Location: ' . $_SERVER['PHP_SELF']);
-}
-if (isset($_POST['comics']) && $_POST['comics'] === "Add to cart") {
-    if (!isset($_SESSION['cart_amount']))
-        $_SESSION['cart_amount'] = 0;
-    $_SESSION['product_price'] = 7.5;
+    $_SESSION['product_price'] = ""; /* Need a function there that could get the price of the item, depending on the name POSTed */
     header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
@@ -48,14 +30,7 @@ if (isset($_POST['empty_cart']) && $_POST['empty_cart'] === "Empty") {
         <?php include ('main_page.php'); ?>
         <?php include ('footer.php'); ?>
     <form action="index.php" method="POST">
-        Thrillers
-        <input type="submit" name="thrillers" value="Add to cart" /><br />
-        Novels
-        <input type="submit" name="novels" value="Add to cart" /><br />
-        Science-fiction
-        <input type="submit" name="sf" value="Add to cart" /><br />
-        Comics
-        <input type="submit" name="comics" value="Add to cart" /><br />
+        <input type="submit" name="cart" value="Add to cart" /><br />
         <input type="submit" name="empty_cart" value="Empty" />
     </form>
 
