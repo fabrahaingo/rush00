1c1d14d27e5d1d4eeb3052e2617ed2e5354e8c1b
Fabien RAHAINGOMANANA <frahaing@e3r2p22.42.fr>
2018-04-01 23:15:10 +0200

    Added some more CSS :)

--- cart_page.php
+++ cart_page.php
@@ -13,20 +13,22 @@ if (file_exists('./db/books') && $all_books && isset($_SESSION['cart_amount']) &
     <link rel="stylesheet" type="text/css" href="stylesheets/cart.css">
 <?php
 /* ===== If the amount of products in the cart is > 0, then display it ===== */
-    if (isset($_SESSION[$book_name]['quantity']) && $_SESSION[$book_name]['quantity'] !== 0) {
+    if (isset($_SESSION[$book_name]['quantity']) && $_SESSION[$book_name]['quantity'] !== 0) { /* ===== TO FIX : display of books for which the title is a number ===== */
         $got_item = 1;
 ?>
     <div class='cart_item'>
 <?php
-        echo "<br><b><font size=\"4\">" . ucfirst($book_name) . "</font></b>";
+        echo "<b><font size=\"4\">" . ucfirst($book_name) . "</font></b>";
         echo "<font size=\"3\"> - Quantity: <i>" . $_SESSION[$book_name]['quantity'] . "</i></font><br>";
         echo "<font size=\"2\">Total for this item - <i>" . ($_SESSION[$book_name]['price'] * $_SESSION[$book_name]['quantity']) . "</i> €</font><br>";
 ?>
     <form action="cart.php" method="POST">
-        <input type="number" name="quantity" min="1" max="100" value="1" /><br />
-        <input type="submit" name="cart" value="Add to cart" /><br />
-        <input type="hidden" name="title_to_remove" value="<?php echo $info['title']; ?>" /></br>
-        <input type="hidden" name="price" value="<?php echo $info['price']; ?>" /></br>
+        <input type="number" name="quantity_add" min="1" max="100" value="1" />
+        <input type="submit" name="add" value="Add to cart" /><br />
+        <input type="number" name="quantity_del" min="1" max="<?php echo $_SESSION[$book_name]['quantity'] ?>" value="1" />
+        <input type="submit" name="del" value="Remove from cart" /><br />
+        <input type="hidden" name="title_item" value="<?php echo $info['title']; ?>" />
+        <input type="hidden" name="price" value="<?php echo $info['price']; ?>" />
     </form>
     </div>
 <?php
@@ -39,7 +41,7 @@ if (file_exists('./db/books') && $all_books && isset($_SESSION['cart_amount']) &
     if (isset($_SESSION['cart_amount'])) {
 ?>
 <div class="total">
-    <p class="price">Total: <?php echo $_SESSION['cart_amount']; ?> €</p>
+    <p>Total: <span class="price"><?php echo $_SESSION['cart_amount']; ?> €</span></p>
     <form action="cart.php" method="POST">
         <input class="place_order" type="submit" name="place_order" value="Place your order" />
     </form>
@@ -51,7 +53,10 @@ if (file_exists('./db/books') && $all_books && isset($_SESSION['cart_amount']) &
 <?php
     }
     if ($got_item === 0)
-        echo ("Your cart is currently empty");
+        echo ("<p style=\"text-align: center; font-family: Arial;\"><font size=\"5\"><b>Your cart is currently empty<b></font><p>");
+}
+else {
+    echo("<p style=\"text-align: center; font-family: Arial;\"><font size=\"5\"><b>Your cart is currently empty<b></font><p>");
 }
 
 ?>
